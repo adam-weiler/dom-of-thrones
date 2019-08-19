25f293e74b32cc2d940811adac1f0ead742b0803
adam-weiler <adam.weiler@yahoo.ca>
2019-08-19 17:01:03 -0400

    Setting up files for Scene2.

---
+++ js/scene2.js
@@ -0,0 +1,33 @@
+function scene2() {
+  console.log('Scene 2:');
+  console.log('And, Action!')
+
+  // Setup Stage
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(() => {
+
+  });
+
+  // Stage direction 2
+
+  frame(() => {
+
+  });
+
+}
